f855ff49731a3a2eb2c79b87ae991a51eb70faba
BLACK-ON-FIRE <137738361+BLACK-ON-FIRE@users.noreply.github.com>
2023-06-27 17:42:00 +0530

    Update start.py

--- VipX/utils/inline/start.py
+++ VipX/utils/inline/start.py
@@ -20,17 +20,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🕹 𝐀𝐃𝐃 𝐌𝐄 𝐁𝐀𝐁𝐘 🕹",
+                text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🦋𝐅𝐄𝐀𝐓𝐔𝐑𝐄🦋",
+                text="ғᴇᴀᴛᴜʀᴇ",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="⚙️𝐒𝐄𝐓𝐓𝐈𝐍𝐆⚙️", callback_data="settings_helper"
+                text="sᴇᴛᴛɪɴɢ", callback_data="settings_helper"
             ),
         ],
      ]
@@ -41,22 +41,22 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✚  𝐀𝐃𝐃 𝐌𝐄 𝐈𝐍 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏  ✚",
+                text="✚  ᴀᴅᴅ ᴍᴇ ɪɴ ᴜʀ ɢʀᴏᴜᴘ  ✚",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         
         ],
         [
             InlineKeyboardButton(
-                text="♦️𝐆𝐑𝐎𝐔𝐏♦️", url=f"https://t.me/{YOUR_GROUP}",
+                text="ɢʀᴏᴜᴘ", url=f"https://t.me/{YOUR_GROUP}",
             ),
             InlineKeyboardButton(
-                text="♦️𝐌𝐎𝐑𝐄♦️", url=f"https://t.me/{YOUR_CHANNEL}",
+                text="ᴍᴏʀᴇ", url=f"https://t.me/{YOUR_CHANNEL}",
             )
         ],
         [
             InlineKeyboardButton(
-                text="⚙️ 𝐇𝐄𝐋𝐏 ⚙️", callback_data="settings_back_helper"
+                text="ʜᴇʟᴘ", callback_data="settings_back_helper"
             )
         ],
      ]
